f553b5bc4d5ce228ca87e62540e006756acd119e
yoraish <yorai.shaoul@gmail.com>
2019-05-01 13:23:26 -0400

    resize script done

--- resize.py
+++ resize.py
@@ -18,15 +18,23 @@ def show_img(arr, text = ""):
     plt.show()
 
 def resize(orig, c,r):
-    show_img(orig)
-    show_img(cv2.resize(orig,  dsize=(r, c), interpolation=cv2.INTER_CUBIC))
+    return cv2.resize(orig,  dsize=(r, c), interpolation=cv2.INTER_CUBIC)
 
     
-def resize_all_in_folder(path, save_to_folder):
+def resize_all_in_folder(path, save_to_folder, r, c):
+    # take all the files in a given directory and save them with the same name, to a new directory - resized to a uniform size
     for filename in os.listdir(path):
         if filename.endswith(".png") or filename.endswith(".jpg"): 
-            print(os.path.join(path, filename))
-            continue
+            print('resizing ', filename)
+            img_path = os.path.join(path, filename)
+
+            #load the image
+            img = np.asarray(Image.open(img_path))
+            resized_img = resize(img, c,r)
+            #save the image
+            cv2.imwrite(os.path.join(save_to_folder, filename), resized_img)
+
+            
         else:
             continue
 
@@ -34,4 +42,4 @@ if __name__ == "__main__":
     # arr = np.asarray(Image.open('sample_meme_2.jpg'))
     # resize(arr,244,244)
 
-    resize_all_in_folder("/Users/yoraish/Dropbox (MIT)/MIT/School/18.065/project/memes","") # change the path to be true to your machine
+    resize_all_in_folder("/Users/yoraish/Dropbox (MIT)/MIT/School/18.065/project/memes","/Users/yoraish/Dropbox (MIT)/MIT/School/18.065/project/memes_resized", 244,244) # change the path to be true to your machine
