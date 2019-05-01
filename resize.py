f0bcd6609c7c8a28600b371e960bb34448a8fd4f
yoraish <yorai.shaoul@gmail.com>
2019-05-01 09:49:47 -0400

    progress on resize

--- resize.py
+++ resize.py
@@ -4,6 +4,10 @@ import matplotlib.pyplot as plt
 import cv2
 from PIL import Image
 
+import os
+
+
+
 def show_img(arr, text = ""):
     if type(arr) != np.ndarray:
         raise Exception('The image could not be showed - not a numpy array')
@@ -18,8 +22,16 @@ def resize(orig, c,r):
     show_img(cv2.resize(orig,  dsize=(r, c), interpolation=cv2.INTER_CUBIC))
 
     
-
+def resize_all_in_folder(path, save_to_folder):
+    for filename in os.listdir(path):
+        if filename.endswith(".png") or filename.endswith(".jpg"): 
+            print(os.path.join(path, filename))
+            continue
+        else:
+            continue
 
 if __name__ == "__main__":
-    arr = np.asarray(Image.open('sample_meme.png'))
-    resize(arr,50,50)
+    # arr = np.asarray(Image.open('sample_meme_2.jpg'))
+    # resize(arr,244,244)
+
+    resize_all_in_folder("/Users/yoraish/Dropbox (MIT)/MIT/School/18.065/project/memes","") # change the path to be true to your machine
